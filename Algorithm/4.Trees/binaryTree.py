f794c692e956b4a5c7cd2d1293a81f678b10b3d9
sssiddharth3030 <sssiddharth3030@gmail.com>
2025-07-28 15:00:17 +0530

    Tree Traversal

--- Algorithm/4.Trees/binaryTree.py
+++ Algorithm/4.Trees/binaryTree.py
@@ -3,3 +3,27 @@ class node:
     self.data = data
     self.left = None
     self.right = None
+    
+def inOrder(root):
+  # left -> root -> right
+  # is used in context of Binary Search Tree
+  inOrder(root.left)
+  print(root.data, ' ')
+  inOrder(root.right)
+  
+def postOrder(root):
+  # left -> right -> root
+  # is used to delete a tree from leaf to root
+  
+  postOrder(root.left)
+  postOrder(root.right)
+  print(root.data, ' ')
+  
+def preOrder(root):
+  # root -> left -> right
+  # is used to copy a tree from root to leaf
+  
+  print(root.data, ' ')
+  preOrder(root.left)
+  preOrder(root.right)
+  
