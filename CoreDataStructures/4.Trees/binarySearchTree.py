46580348769631154a89b0bd5542f131c5927df2
sssiddharth3030 <sssiddharth3030@gmail.com>
2025-07-28 17:07:54 +0530

    Binary Search Tree Object

---
+++ CoreDataStructures/4.Trees/binarySearchTree.py
@@ -0,0 +1,19 @@
+# Binary Search Tree
+
+# Root should be the biggest number comparing left branch
+# Root should be smaller than right branch
+
+class node:
+  def __ini__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+
+def insert(data):
+  pass
+
+def display():
+  pass
+
+def remove(data):
+  pass
