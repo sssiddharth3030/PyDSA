158b28c8f77684d25addba41261a061f945942e9
sssiddharth3030 <sssiddharth3030@gmail.com>
2025-07-28 15:56:05 +0530

    Stack

---
+++ CoreDataStructures/5.Stacks/stacks.py
@@ -0,0 +1,26 @@
+# stack is LIFO - Last in, First Out
+
+#peek - look at top most element without removing it
+def peek(stack):
+  if stack:
+    return stack[-1]
+  return None
+  
+stack = []
+
+# add elements to end
+stack.append('a')
+stack.append('b')
+stack.append('c')
+stack.append('d')
+stack.append('e')
+
+print(stack)
+print(peek(stack))
+
+#remove elements from end
+stack.pop()
+stack.pop()
+
+print(stack)
+print(peek(stack))
